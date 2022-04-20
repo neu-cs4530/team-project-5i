1e3f0df57ab9b72c6b5727410188c434941f66a6
QuantumCici <72942065+QuantumCici@users.noreply.github.com>
2022-04-20 17:48:28 -0400

    Add get methods for test purposes
    It's a resubmission with proper description.

--- frontend/src/classes/TextConversation.ts
+++ frontend/src/classes/TextConversation.ts
@@ -8,6 +8,17 @@ import { Socket } from 'socket.io-client';
  */
 export default class TextConversation {
 
+  get socket(): Socket {
+    return this._socket
+  }
+
+  get callbacks(): MessageCallback[]  {
+    return this._callbacks
+  }
+
+  get authorName(): string {
+    return this._authorName
+  }
 
   private _socket: Socket;
 
